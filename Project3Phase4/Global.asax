bb22da74dcca41d9115cb7a383cf38e85ddcf04a
Rashmi Priya <priye98priya@gmail.com>
2024-02-21 07:30:44 +0530

    Initial Commit

---
+++ Project3Phase4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project3Phase4.MvcApplication" Language="C#" %>
---
+++ Project3Phase4/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Project3Phase4.Models
+{
+    [Table("EmployeeP3Ph4")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+    }
+}
